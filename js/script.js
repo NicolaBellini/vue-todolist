2c44718456c9e38b0bbd0f4d5e8c8d1f693e6f00
NicolaBellini <bellininicola4366@gmail.com>
2024-03-13 13:33:44 +0100

    [add] ToDoList object array

--- js/script.js
+++ js/script.js
@@ -3,10 +3,27 @@ const{createApp}=Vue
 createApp({
   data() {
     return {
-      
+      toDoList:[
+        {
+          text:'todo-thing',
+          done: true,
+        },
+        {
+          text:'todo-thing',
+          done: false,
+        },
+        {
+          text:'todo-thing',
+          done: false,
+        },
+        {
+          text:'todo-thing',
+          done: true,
+        },
+      ],
     }
   },
-  
+
   mounted() {
     console.log('ciao');
   },
